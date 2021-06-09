d2ddaba4284aca57d47b4f0b5cfa287e4788fc3c
M.S Vinayak <mailvizzard@gmail.com>
2021-06-09 16:28:47 +0530

    jdfb

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -95,7 +95,6 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                       ),
                     ),
-
                   ],
                 ),
               ),
